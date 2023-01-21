2b624b032badf7fffa9e2680bb20bbb948049760
Daniil Okonskiy <dofeternity@gmail.com>
2023-01-21 21:07:06 +0600

    Implemented Command Handling

---
+++ SM-Post/Post.Cmd/Post.Cmd.Api/DependencyInjection.cs
@@ -0,0 +1,28 @@
+﻿using System.Reflection;
+using CQRS.Core.Commands;
+using CQRS.Core.Infrastructure;
+using Post.Cmd.Api.Commands;
+using Post.Cmd.Infrastructure.Dispatchers;
+
+namespace Post.Cmd.Api;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddCommands(this IServiceCollection services)
+    {
+        var commandHandler = services.BuildServiceProvider().GetRequiredService<ICommandHandler>();
+
+        var dispatcher = new CommandDispatcher();
+
+        dispatcher.RegisterHandler<AddCommentCommand>(commandHandler.HandleAsync);
+        dispatcher.RegisterHandler<DeletePostCommand>(commandHandler.HandleAsync);
+        dispatcher.RegisterHandler<EditCommentCommand>(commandHandler.HandleAsync);
+        dispatcher.RegisterHandler<EditMessageCommand>(commandHandler.HandleAsync);
+        dispatcher.RegisterHandler<LikePostCommand>(commandHandler.HandleAsync);
+        dispatcher.RegisterHandler<NewPostCommand>(commandHandler.HandleAsync);
+        dispatcher.RegisterHandler<RemoveCommentCommand>(commandHandler.HandleAsync);
+        services.AddSingleton<ICommandDispatcher>(_ => dispatcher);
+
+        return services;
+    }
+}
--- SM-Post/Post.Cmd/Post.Cmd.Api/Program.cs
+++ SM-Post/Post.Cmd/Post.Cmd.Api/Program.cs
@@ -1,8 +1,15 @@
 using CQRS.Core.Domain;
+using CQRS.Core.Handlers;
 using CQRS.Core.Infrastructure;
+using Post.Cmd.Api.Commands;
+using Post.Cmd.Domain.Aggregates;
 using Post.Cmd.Infrastructure.Configs;
+using Post.Cmd.Infrastructure.Dispatchers;
+using Post.Cmd.Infrastructure.Handlers;
 using Post.Cmd.Infrastructure.Repositories;
 using Post.Cmd.Infrastructure.Stores;
+using Microsoft.Extensions.DependencyInjection;
+using Post.Cmd.Api;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -16,6 +23,11 @@ services.AddSwaggerGen();
 services.Configure<MongoDbConfig>(builder.Configuration.GetSection($"{nameof(MongoDbConfig)}"));
 services.AddScoped<IEventStoreRepository, EventStoreRepository>();
 services.AddScoped<IEventStore, EventStore>();
+services.AddScoped<IEventSourcingHandler<PostAggregate>, EventSourcingHandler>();
+services.AddScoped<ICommandHandler, CommandHandler>();
+
+// Register command handler methods
+services.AddCommands();
 
 var app = builder.Build();
 
